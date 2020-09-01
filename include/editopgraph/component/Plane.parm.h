1201c71285b417f8e4cbf2146f3acbd5b50aff61
zz <xzrunner@gmail.com>
2020-09-01 08:36:22 +0800

    rename

--- include/editopgraph/component/Plane.parm.h
+++ include/editopgraph/component/Plane.parm.h
@@ -2,5 +2,5 @@
 #error "You must define PARAM_INFO macro before include this file"
 #endif
 
-PARAM_INFO(Normal,   Float3, normal, m_normal, (0, 1, 0))
-PARAM_INFO(Distance, Float,  dist,   m_dist,   (0))
+PARAM_INFO(Normal,   sm::vec3, normal, m_normal, (0, 1, 0))
+PARAM_INFO(Distance, float,  dist,   m_dist,   (0))
--- include/editopgraph/component/Ray.parm.h
+++ include/editopgraph/component/Ray.parm.h
@@ -2,5 +2,5 @@
 #error "You must define PARAM_INFO macro before include this file"
 #endif
 
-PARAM_INFO(StartPos, Float3, start_pos, m_start_pos, ())
-PARAM_INFO(EndPos,   Float3, end_pos,   m_end_pos,   ())
+PARAM_INFO(StartPos, sm::vec3, start_pos, m_start_pos, ())
+PARAM_INFO(EndPos,   sm::vec3, end_pos,   m_end_pos,   ())
